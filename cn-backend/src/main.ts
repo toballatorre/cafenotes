2cc17820c939ba1c30142d0dc80b35a3c10a964c
Tobal Latorre <tobal.latorre@gmail.com>
2025-10-21 13:14:57 -0300

    Dockerize Backend and DB - DB PstgreSQL 16 - Backend Nestjs - returns a hello worls using get

--- cn-backend/src/main.ts
+++ cn-backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const port = process.env.NEST_PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
